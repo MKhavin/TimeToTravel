a693488215879afebc144497e601f9d7aed98a06
MichaelK <havinmichael@gmail.com>
2022-06-01 23:13:18 +0300

    Initial Commit

---
+++ TimeToTravel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TimeToTravel
+//
+//  Created by Michael Khavin on 01.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
